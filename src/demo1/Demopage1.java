2cafb07f5c238e6946b0c6427f322ca3fb42ee4c
yeshwanthkancherla <yeshwanth.kancherla@clientservertech.com>
2023-07-20 14:54:54 +0530

    First Commit

---
+++ src/demo1/Demopage1.java
@@ -0,0 +1,12 @@
+package demo1;
+
+public class Demopage1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to GIT");
+
+	}
+
+}
